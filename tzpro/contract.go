70e0d1f206ceae7156b89778e05610bc9eac6217
Alexander Eichhorn <alex@blockwatch.cc>
2022-11-02 14:52:55 +0400

    Make metadata writable

--- tzpro/contract.go
+++ tzpro/contract.go
@@ -20,38 +20,45 @@ import (
 )
 
 type Contract struct {
-	RowId         uint64              `json:"row_id,omitempty"`
-	AccountId     uint64              `json:"account_id,omitempty"`
-	Address       tezos.Address       `json:"address"`
-	CreatorId     uint64              `json:"creator_id,omitempty"`
-	Creator       tezos.Address       `json:"creator"`
-	BakerId       uint64              `json:"baker_id,omitempty" tzpro:"notable"`
-	Baker         tezos.Address       `json:"baker"              tzpro:"notable"`
-	FirstSeen     int64               `json:"first_seen"`
-	LastSeen      int64               `json:"last_seen"`
-	FirstSeenTime time.Time           `json:"first_seen_time"`
-	LastSeenTime  time.Time           `json:"last_seen_time"`
-	StorageSize   int64               `json:"storage_size"`
-	StoragePaid   int64               `json:"storage_paid"`
-	Script        *micheline.Script   `json:"script,omitempty"`
-	Storage       *micheline.Prim     `json:"storage,omitempty"`
-	InterfaceHash string              `json:"iface_hash"`
-	CodeHash      string              `json:"code_hash"`
-	StorageHash   string              `json:"storage_hash"`
-	Features      []string            `json:"features"`
-	Interfaces    []string            `json:"interfaces"`
-	CallStats     map[string]int      `json:"call_stats"`
-	NCallsSuccess int                 `json:"n_calls_success"     tzpro:"notable"`
-	NCallsFailed  int                 `json:"n_calls_failed"      tzpro:"notable"`
-	Bigmaps       map[string]int64    `json:"bigmaps,omitempty"   tzpro:"notable"`
-	Metadata      map[string]Metadata `json:"metadata,omitempty"  tzpro:"notable"`
+	RowId         uint64               `json:"row_id,omitempty"`
+	AccountId     uint64               `json:"account_id,omitempty"`
+	Address       tezos.Address        `json:"address"`
+	CreatorId     uint64               `json:"creator_id,omitempty"`
+	Creator       tezos.Address        `json:"creator"`
+	BakerId       uint64               `json:"baker_id,omitempty" tzpro:"notable"`
+	Baker         tezos.Address        `json:"baker"              tzpro:"notable"`
+	FirstSeen     int64                `json:"first_seen"`
+	LastSeen      int64                `json:"last_seen"`
+	FirstSeenTime time.Time            `json:"first_seen_time"`
+	LastSeenTime  time.Time            `json:"last_seen_time"`
+	StorageSize   int64                `json:"storage_size"`
+	StoragePaid   int64                `json:"storage_paid"`
+	Script        *micheline.Script    `json:"script,omitempty"`
+	Storage       *micheline.Prim      `json:"storage,omitempty"`
+	InterfaceHash string               `json:"iface_hash"`
+	CodeHash      string               `json:"code_hash"`
+	StorageHash   string               `json:"storage_hash"`
+	Features      []string             `json:"features"`
+	Interfaces    []string             `json:"interfaces"`
+	CallStats     map[string]int       `json:"call_stats"`
+	NCallsSuccess int                  `json:"n_calls_success"     tzpro:"notable"`
+	NCallsFailed  int                  `json:"n_calls_failed"      tzpro:"notable"`
+	Bigmaps       map[string]int64     `json:"bigmaps,omitempty"   tzpro:"notable"`
+	Metadata      map[string]*Metadata `json:"metadata,omitempty"  tzpro:"notable"`
 
 	columns []string `json:"-"`
 }
 
-func (c Contract) Meta() *Metadata {
-	m := c.Metadata[c.Address.String()]
-	return &m
+func (c *Contract) Meta() *Metadata {
+	m, ok := c.Metadata[c.Address.String()]
+	if !ok {
+		m = NewMetadata(c.Address)
+		if c.Metadata == nil {
+			c.Metadata = make(map[string]*Metadata)
+		}
+		c.Metadata[c.Address.String()] = m
+	}
+	return m
 }
 
 type ContractList struct {
